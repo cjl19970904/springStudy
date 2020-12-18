bb87ab9d7eb362b1bf73783aa0c0b63d45132e8c
jlchen <http//git.ittx.com.cn/jlchen>
2020-12-18 09:52:53 +0800

    代码

--- spring-cjl-test/src/main/java/cjl/spring/test/Test4.java
+++ spring-cjl-test/src/main/java/cjl/spring/test/Test4.java
@@ -3,6 +3,7 @@ package cjl.spring.test;
 import cjl.spring.app.A;
 import cjl.spring.app.AppConfig;
 import cjl.spring.app.CjlScan;
+import cjl.spring.beanDefinition.CjlBeanPostProcessor;
 import cjl.spring.beanDefinition.CustomScanner;
 import cjl.spring.service.IndexService;
 import org.springframework.beans.factory.config.BeanDefinition;
@@ -18,10 +19,11 @@ import org.springframework.stereotype.Repository;
 
 public class Test4 {
 	public static void main(String[] args) {
-		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
-//		ac.register(AppConfig.class);
+		AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(/*AppConfig.class*/);
+		ac.register(AppConfig.class);
 //
-//		ac.refresh();
+		ac.addBeanFactoryPostProcessor(new CjlBeanPostProcessor());
+		ac.refresh();
 		System.out.println(ac.getBeanDefinition("a").getClass());
 
 //		System.out.println(ac.getBean(AppConfig.class));
